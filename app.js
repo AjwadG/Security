1bfb1565c7f2ed028cee00724d56c3bc0efc153b
AjwadG <ajwadelgandouz@yahoo.com>
2023-05-30 21:45:32 +0200

    using passport with mongoose

--- app.js
+++ app.js
@@ -3,14 +3,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds =10;
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const { functions } = require('lodash');
+
 
 const app = express();
 
  app.set("view engine", "ejs")
  app.use(bodyParser.urlencoded({extended: true}));
  app.use(express.static("public"))
+
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
  main().catch(err => console.log(err));
 
@@ -23,9 +38,16 @@ async function main (){
         email: String,
         password: String
     })
-    
 
+    userSchema.plugin(passportLocalMongoose);
+    
     const User = new mongoose.model("User", userSchema);
+
+
+    passport.use(User.createStrategy());
+
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
 
 
 
@@ -34,44 +56,64 @@ async function main (){
     })
 
 
-    app.route("/login")
-        .get(function(req, res){
-            res.render("login", {msg: "",})
-        })
-        .post(async function(req, res){
-            await User.findOne({email: req.body.username})
-            .then(table => {
-            bcrypt.compare(req.body.password, table.password, async function(err, result) {
-                if (result === false) res.render("login", {msg: "Wrong username or password"})
-                else res.render("secrets")
-            });
-            })
-        })
+    app.get("/login", function(req, res){
+        res.render("login", {msg: ""})
+    })
+
+    app.post('/login',
+    passport.authenticate('local', { successRedirect: '/secrets', failWithError: true }),
+    function(err, req, res, next) {
+        res.render("login", {msg: "Wrong Username or Password"})
+    });
+
+
+
+    // app.post("/login",async function(req, res){
+    //         const user = new User({
+    //             username : req.body.username,
+    //             password : req.body.password
+    //         })
+    //       req.login(user, function(err, a) {
+    //         console.log(user);
+    //         if (err) { 
+    //             console.log(err); 
+    //             res.render("login", {msg: "Wrong Username or Password"})
+    //         } else {
+                
+    //         }
+            
+    //     });
+    //     })
 
     app.route("/register")
         .get
         (function(req, res){
         res.render("register")
         })
-
         .post(async function(req, res){
-            bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-            
-            const newUser = User({
-                email: req.body.username,
-                password: hash
-            }) 
-            newUser.save().then(() => res.render("secrets"))
-            .catch(err => console.log(err))
-
-            });
+            const username = req.body.username;
+            const password = req.body.password
+            User.register({username:username}, password, function(err, user) {
+                if (err) {
+                    console.log(err);
+                    res.redirect("/register");
+                  } else {
+                        passport.authenticate("local")(req, res , function() {
+                            res.redirect("/secrets");
+                        });
+                    }
+            })
         });
-    
+        
 
-
-        app.get("/logout", function(req, res){
-            res.redirect("/")
+        app.get("/secrets", function(req, res){
+            if (req.isAuthenticated()){
+            res.render("secrets");
+            } else {
+                res.redirect("/login");
+            }
         })
+
 
         app.route("/submit")
         .get
@@ -83,7 +125,12 @@ async function main (){
         });
     
         app.get("/logout", function(req, res){
-            res.redirect("/")
+            req.logout(function(err) {
+                if (err) { console.log(err); } else {
+                    res.redirect('/');
+                }
+                
+            })
         })
     
 
